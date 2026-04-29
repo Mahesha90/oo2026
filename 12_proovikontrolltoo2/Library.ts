d4f79f0ce464c5469f2699cc0589a2bb24c90b51
Mahesha De Silva <62265187+Mahesha90@users.noreply.github.com>
2026-04-29 10:27:00 +0000

    update

---
+++ 12_proovikontrolltoo2/Library.ts
@@ -0,0 +1,41 @@
+class LibraryItem{
+    id: string;
+    title: string;
+    author: string;
+    year: number;
+
+    constructor(id: string, title:string, author:string, year:number){
+    //Removes the whitespaces from both ends of the string
+    //=== checking whether this is empty after removing spaces
+        if(id.trim()==="") throw new Error("ID cannot be emprty");
+        this.id=id;
+        this.title=title;
+        this.author=author;
+        this.year=year;
+    }
+
+    getId(): string{return this.id;}
+    getTitle(): string{return this.title;}
+    getAuthor(): string{return this.author;}
+    getYear(): number{return this.year;}
+    getSummary(): string{return `[Item] ${this.title}`;}
+
+}
+
+//-------------------------------Book------------------------------------
+class Book extends LibraryItem{
+    pages: number;
+    ISBN: string;
+
+    constructor(id:string, title:string, author:string, year:number, pages:number, ISBN:string
+    ){
+        super(id, title, author, year);
+        if(pages <=0) throw new Error("Pages must be positive");
+        this.pages=pages;
+        this.ISBN=ISBN;
+    }
+
+    getSummary(): string {
+        return `[Book] ${this.title} (${this.year})`;
+    }
+}
